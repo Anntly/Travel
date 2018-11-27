18f7c485286a3f48166a709e309ad50e0f6f17a8
Anntly <btzhang@foxmail.com>
2018-11-27 23:00:11 +0800

    icon init

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,6 +2,7 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 
 </template>
@@ -9,12 +10,14 @@
 <script>
 import HomeHeader from './components/Header';// 引入头部
 import HomeSwiper from './components/Swiper';// 引入录播图
+import HomeIcons from './components/Icons';// 引入录播图
 
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
+    HomeIcons,
   }
 };
 </script>
